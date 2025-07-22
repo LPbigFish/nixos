5b6fd32e3fba74276dc5b0aa72f2c02eab6ff28a
LPbigFish <lpbyblock@gmail.com>
2025-07-22 08:54:57 +0000

    trying to fix this stuff

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -88,5 +88,4 @@
   ];
 
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
--- minimal/configuration.nix
+++ minimal/configuration.nix
@@ -1,5 +1,12 @@
-{ config, pkgs, lib, ... }: {
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
   imports = [
+    # Include the results of the hardware scan.
     ./disk-config.nix
   ];
 
@@ -8,4 +15,68 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8";
+    LC_NAME = "cs_CZ.UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8";
+    LC_TIME = "cs_CZ.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "cz";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "cz-lat2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.lpbigfish = {
+    isNormalUser = true;
+    description = "LPbigFish";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    createHome = true;
+    initialPassword = 1234;
+    packages = with pkgs; [ ];
+  };
+
+  users.users.root.initialPassword = 1234;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    nixfmt-rfc-style
+  ];
+
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
