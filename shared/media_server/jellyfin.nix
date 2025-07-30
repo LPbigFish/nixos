1893a8ab14c1acd164e69c8ced0a4ed94715174d
LPbigFish <lpbyblock@gmail.com>
2025-07-30 18:58:28 +0000

    that was not a good idea

--- shared/media_server/jellyfin.nix
+++ shared/media_server/jellyfin.nix
@@ -1,13 +1,4 @@
 { ... }: {
-  users.users.jellyfin = {
-    isSystemUser = true;
-    description = "Jellyfin user account";
-    extraGroups = [
-      "networkmanager"
-    ];
-    createHome = true;
-  };
-
   services.jellyfin = {
     enable = true;
     openFirewall = true;
