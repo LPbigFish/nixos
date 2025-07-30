87982624731188dbd82d59b760c98c0786f8503a
LPbigFish <lpbyblock@gmail.com>
2025-07-30 21:04:39 +0000

    ugh

--- orangepi5pro/led-overlay.nix
+++ orangepi5pro/led-overlay.nix
@@ -6,22 +6,22 @@
         /dts-v1/;
         /plugin/;
 
-        // Turn off blue_led@1
-        fragment@0 {
-          target-path = "/gpio-leds/blue_led@1";
-          __overlay__ {
-            linux,default-trigger = "none";
-            default-state = "off";
-          };
-        };
+        / {
+            fragment@0 {
+                target-path = "/gpio-leds/blue_led@1";
+                __overlay__ {
+                    linux,default-trigger = "none";
+                    default-state = "off";
+                };
+            };
 
-        // Turn off green_led@2
-        fragment@1 {
-          target-path = "/gpio-leds/green_led@2";
-          __overlay__ {
-            linux,default-trigger = "none";
-            default-state = "off";
-          };
+            fragment@1 {
+                target-path = "/gpio-leds/green_led@2";
+                __overlay__ {
+                    linux,default-trigger = "none";
+                    default-state = "off";
+                };
+            };
         };
       '';
     }
