dbca2a6c4b7dba1a6e819638fe889d8076395c88
LPbigFish <lpbyblock@gmail.com>
2025-07-30 18:35:42 +0000

    added jellyfin user

--- shared/media_server/jellyfin.nix
+++ shared/media_server/jellyfin.nix
@@ -1,4 +1,14 @@
 { ... }: {
+  users.users.jellyfin = {
+    isNormalUser = true;
+    description = "Jellyfin user account";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    createHome = true;
+  };
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
