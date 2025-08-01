1b9689c08b382cc345721482f303b8d4ed9dc234
LPbigFish <lpbyblock@gmail.com>
2025-08-01 20:21:31 +0000

    fixing transcoding issues

--- shared/media_server/jellyfin.nix
+++ shared/media_server/jellyfin.nix
@@ -1,6 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
+
+  users.users.jellyfin.extraGroups = [ "video" "render" ];
+
+  environment.systemPackages = with pkgs; [ v4l-utils ffmpeg ];
 }
