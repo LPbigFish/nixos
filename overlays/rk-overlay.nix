061cbcbce833b36959c16685178239e61bf9f88e
LPbigFish <lpbyblock@gmail.com>
2025-08-01 21:35:17 +0000

    be

--- overlays/rk-overlay.nix
+++ overlays/rk-overlay.nix
@@ -1,37 +1,43 @@
 final: prev:
 let
-  inherit (prev) lib stdenv fetchFromGitHub cmake pkg-config libdrm;
+  inherit (prev) lib stdenv fetchFromGitHub cmake ninja pkg-config libdrm;
 
   rockchip-mpp = stdenv.mkDerivation rec {
     pname = "rockchip-mpp";
-    # Pick a known-good MPP rev; update if you like.
     version = "1.0.9";
     src = fetchFromGitHub {
       owner = "rockchip-linux";
       repo  = "mpp";
-      rev   = version;
-      # First build will tell you the wanted hash; paste it back here.
+      rev   = version;  # note: no leading 'v'
       sha256 = "sha256-+1Gnx7n9nZVVt0S/hZEzXupADPX0JRmTGD1XBhLMZ7o=";
     };
 
-    nativeBuildInputs = [ cmake pkg-config ];
-    buildInputs       = [ libdrm ];
+    nativeBuildInputs = [ cmake ninja pkg-config ];
+    buildInputs = [ libdrm ];
+
     cmakeFlags = [
-      "-DCMAKE_BUILD_TYPE=Release"
       "-DRKPLATFORM=ON"
-      "-DBUILD_TEST=OFF"
-      "-DBUILD_DOC=OFF"
+      "-DCMAKE_BUILD_TYPE=Release"
+      "-DCMAKE_INSTALL_LIBDIR=lib"
+      "-DCMAKE_INSTALL_INCLUDEDIR=include"
     ];
+
+    # Fix double-slash paths in the generated .pc files
+    postFixup = ''
+      for pc in "$out"/lib/pkgconfig/*.pc; do
+        [ -e "$pc" ] || continue
+        substituteInPlace "$pc" \
+          --replace "libdir=\${prefix}//" "libdir=\${prefix}/" \
+          --replace "includedir=\${prefix}//" "includedir=\${prefix}/"
+      done
+    '';
   };
 in {
-  # Expose the libraries (names are your choice)
   rockchip-mpp = rockchip-mpp;
 
-  # Teach jellyfin-ffmpeg to use them
   jellyfin-ffmpeg = prev.jellyfin-ffmpeg.overrideAttrs (old: {
-    buildInputs = (old.buildInputs or []) ++ [ rockchip-mpp libdrm pkg-config ];
-    configureFlags = (old.configureFlags or []) ++ [
-      "--enable-rkmpp"
-    ];
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkg-config ];
+    buildInputs       = (old.buildInputs or [])       ++ [ rockchip-mpp libdrm ];
+    configureFlags    = (old.configureFlags or [])    ++ [ "--enable-rkmpp" ];
   });
 }
