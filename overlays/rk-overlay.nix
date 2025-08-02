12475adba2d82e87660b34ea3a002fc427c0cb16
LPbigFish <lpbyblock@gmail.com>
2025-08-02 07:22:16 +0000

    t

--- overlays/rk-overlay.nix
+++ overlays/rk-overlay.nix
@@ -45,16 +45,33 @@ let
       done
     '';
   };
+
+  librga = stdenv.mkDerivation rec {
+    pname = "librga";
+    version = "v1.10.4";  # or another stable tag
+    src = fetchFromGitHub {
+      owner = "airockchip";
+      repo  = "librga";
+      rev   = version;
+      sha256 = lib.fakeSha256;  # replace after first build
+    };
+    nativeBuildInputs = [ cmake ninja pkg-config ];
+    buildInputs = [ libdrm ];
+    cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DBUILD_SHARED_LIBS=ON" ];
+  };
 in
 {
   rockchip-mpp = rockchip-mpp;
+
+  libra = librga;
 
   jellyfin-ffmpeg = prev.jellyfin-ffmpeg.overrideAttrs (old: {
     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkg-config ];
     buildInputs = (old.buildInputs or [ ]) ++ [
       rockchip-mpp
       libdrm
+      librga
     ];
-    configureFlags = (old.configureFlags or [ ]) ++ [ "--enable-rkmpp" ];
+    configureFlags = (old.configureFlags or [ ]) ++ [ "--enable-rkmpp" "--enable-rkrga" ];
   });
 }
