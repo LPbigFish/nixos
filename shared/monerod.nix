a07fc49f856b8041c2c71d449e613e3e940184b2
LPbigFish <lpbyblock@gmail.com>
2025-09-15 18:54:09 +0000

    edited config of monero

--- shared/monerod.nix
+++ shared/monerod.nix
@@ -12,7 +12,11 @@
       rpc-restricted-bind-port=18089
       proxy=127.0.0.1:9050
       tx-proxy=tor,127.0.0.1:9050
-      out-peers=4
+      out-peers=64
+
+      anonymous-inbound=127.0.0.1:18080,127.0.0.1:9050
+
+      rpc-restricted-bind-port=18089
     '';
   };
 }
