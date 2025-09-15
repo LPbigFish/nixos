1e3bc63dfb8c6550831589616116511a89a03f7a
LPbigFish <lpbyblock@gmail.com>
2025-09-15 19:12:19 +0000

    Type B

--- shared/monerod.nix
+++ shared/monerod.nix
@@ -10,18 +10,15 @@
     };
 
     extraConfig = ''
-      # Tor-only
       proxy=127.0.0.1:9050
       tx-proxy=tor,127.0.0.1:9050,enable_noise=1
       out-peers=64
 
-      # Make the local P2P listener match your anonymous-inbound target
       p2p-bind-ip=127.0.0.1
-      p2p-bind-port=18080
+      p2p-bind-port=18090
       hide-my-port=1
 
-      # Tor hidden service that forwards to the local listener above
-      anonymous-inbound=127.0.0.1:18080,127.0.0.1:9050
+      anonymous-inbound=127.0.0.1:18090,127.0.0.1:9050
     '';
   };
 }
