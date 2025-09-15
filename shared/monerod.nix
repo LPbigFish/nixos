1cea1ff03c3772010dd5304f20b34c76fce6b0dd
LPbigFish <lpbyblock@gmail.com>
2025-09-15 19:30:43 +0000

    giving monero access to tor

--- shared/monerod.nix
+++ shared/monerod.nix
@@ -1,7 +1,4 @@
-{ config, ... }:
-let
-  onionLink = builtins.readFile config.sops.secrets.onionLink.path;
-in
+{ ... }:
 {
   services.monero = {
     enable = true;
@@ -21,7 +18,9 @@ in
       p2p-bind-port=18090
       hide-my-port=1
 
-      anonymous-inbound=${onionLink}:18090,127.0.0.1:9050
+      tor-control=127.0.0.1:9051
+
+      anonymous-inbound=127.0.0.1:18090,127.0.0.1:9050
     '';
   };
 }
--- shared/tor_services.nix
+++ shared/tor_services.nix
@@ -1,19 +1,29 @@
-{ ... }: {
+{ ... }:
+{
   services.tor = {
     enable = true;
 
     client.enable = true;
 
-    relay.onionServices.monerod-rpc = {
-      version = 3;
-      map = [{
-        # Monerod
-        port = 18089;
-        target = {
-          addr = "127.0.0.1";
-          port = 18089;
-        };
-      }];
+    settings = {
+      SocksPort = 9050;
+      ControlPort = 9051;
+      CookieAuthentication = true;
+      CookieAuthFileGroupReadable = true;
     };
+
+    /* relay.onionServices.monerod-rpc = {
+      version = 3;
+      map = [
+        {
+          # Monerod
+          port = 18089;
+          target = {
+            addr = "127.0.0.1";
+            port = 18089;
+          };
+        }
+      ];
+    }; */
   };
 }
