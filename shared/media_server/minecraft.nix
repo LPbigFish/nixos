4e0d65fbb20ff5d6894f3fedacf2b0276da0d50c
LPbigFish <lpbyblock@gmail.com>
2025-11-22 12:47:02 +0000

    Added Shops for Multiplayer

--- shared/media_server/minecraft.nix
+++ shared/media_server/minecraft.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }:
 let
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/LPbigFish/FabricMods/fb3f949404ace81b1eb4bea0c437649ce3970058/pack.toml";
-    packHash = "sha256-MtfBhCAZwNH+9Cs8d/6LmKIQg/nbzj3ht9el2NzfdFw=";
+    url = "https://raw.githubusercontent.com/LPbigFish/FabricMods/f62012acee4010b526b3a946cca30ec1001742a7/pack.toml";
+    packHash = "sha256-1r5m6l3rhnpjvfmw6sf5qngby50cwl3np31j0asd9mr4n6yhbx45";
   };
 in {
   services.minecraft-servers = {
