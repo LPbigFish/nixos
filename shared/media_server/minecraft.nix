64f1c657d8b3ecc3a5d091834f0bf4efe2ff2f42
LPbigFish <lpbyblock@gmail.com>
2025-11-22 13:41:50 +0000

    Removed Xray

--- shared/media_server/minecraft.nix
+++ shared/media_server/minecraft.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   modpack = pkgs.fetchPackwizModpack {
-    url = "https://raw.githubusercontent.com/LPbigFish/FabricMods/611d51c64d25ec16703308590a3d8369ddc7507e/pack.toml";
-    packHash = "sha256-1SwHhNF9eFH6v8R0SjOb5pWZnQF+GL4ttpINEqwjwJM=";
+    url = "https://raw.githubusercontent.com/LPbigFish/FabricMods/2252cca5909f84757501388c862809f194b4e8b5/pack.toml";
+    packHash = lib.fakeSha256;
   };
 in {
   services.minecraft-servers = {
