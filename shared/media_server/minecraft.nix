06060e244e406d24752533f22ddf3b2c10b4a2c9
LPbigFish <lpbyblock@gmail.com>
2025-11-22 12:36:43 +0000

    updated hash

--- shared/media_server/minecraft.nix
+++ shared/media_server/minecraft.nix
@@ -2,7 +2,7 @@
 let
   modpack = pkgs.fetchPackwizModpack {
     url = "https://raw.githubusercontent.com/LPbigFish/FabricMods/fb3f949404ace81b1eb4bea0c437649ce3970058/pack.toml";
-    packHash = lib.fakeSha256;
+    packHash = "sha256-MtfBhCAZwNH+9Cs8d/6LmKIQg/nbzj3ht9el2NzfdFw=";
   };
 in {
   services.minecraft-servers = {
@@ -21,7 +21,7 @@ in {
       };
 
       serverProperties = {
-        difficulty = 2;
+        difficulty = 3;
         max-players = 12;
         spawn-protection = 0;
         level-seed = "-7266280065385116388";
