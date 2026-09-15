ceb446bd143ab8a910d0bc5d6d722f6778db296d
LPbigFish <lpbyblock@gmail.com>
2026-09-15 08:06:06 +0200

    Flake bump

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -41,7 +41,7 @@ in
       hunspell
       hunspellDicts.cs_CZ
       # android-studio
-      davinci-resolve
+      #davinci-resolve
       resolve-convert
       obsidian
       opencode
@@ -53,7 +53,6 @@ in
       postman
       beeper
       qbittorrent
-      freecad-wayland
     ])
     ++ (with pkgs.jetbrains; [
       idea
